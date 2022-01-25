43f609d076a367197130cb5fce6a16789ce08fef
sahanasan <sahanasan1999@gmail.com>
2022-01-25 00:14:48 -0500

    first commit

---
+++ src/Day_22_Generics/GenericClassDemo.java
@@ -0,0 +1,27 @@
+package Day_22_Generics;
+
+
+class GenericTest<T>
+{
+	T obj;
+	void add(T obj)
+	{
+		this.obj=obj;
+	}
+	
+	T get()
+	{
+		return obj;
+	}
+}
+
+public class GenericClassDemo {
+
+	public static void main(String[] args) {
+		GenericTest<Integer> obj=new GenericTest<Integer>();
+		obj.add(2);
+		System.out.println(obj.get());
+	}
+
+}
+
